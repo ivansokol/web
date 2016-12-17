fbd7182afa862f142f8d8a5ed9b973636f388c3d
robot <robot@robot.com>
2016-12-17 23:59:23 +0300

    add stylelint

--- tasks/less.js
+++ tasks/less.js
@@ -6,7 +6,7 @@ const notify = require('gulp-notify');
 const debug = require('gulp-debug');
 const sourcemaps = require('gulp-sourcemaps');
 const combine = require('stream-combiner2').obj;
-const csslint = require('gulp-csslint');
+const gulpStylelint = require('gulp-stylelint');
 const autoprefixer = require('gulp-autoprefixer');
 const minifyCSS = require('gulp-minify-css');
 const rename = require('gulp-rename');
@@ -25,9 +25,13 @@ module.exports = function(options) {
         less(),
         autoprefixer({browsers: ['last 2 versions']}),
         csscomb(),
-        csslint(),
-        csslint.formatter(),
-        //csslint.formatter('fail'),
+        /*gulpStylelint({
+            failAfterError: true,
+            debug: true,
+            reporters: [
+                        {formatter: 'string', console: true}
+             ]
+        }),*/
         gulpIf(!options.debug, minifyCSS('')),
         gulpIf(!options.debug, rename({suffix: '.min'})),
         gulpIf(options.debug, sourcemaps.write()),
