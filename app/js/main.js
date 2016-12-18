725bd342858c0db61de4e934575da39e86bb83a9
robot <robot@robot.com>
2016-12-18 16:38:03 +0300

    add new

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,3 @@
 'use strict';
 
-// var a = 0;
+//  var a = 0;
--- www/js/main.js
+++ www/js/main.js
@@ -1,5 +1,5 @@
 'use strict';
 
-// var a = 0;
+//  var a = 0;
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIid1c2Ugc3RyaWN0JztcblxuLy8gdmFyIGEgPSAwO1xuIl0sImZpbGUiOiJtYWluLmpzIn0=
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIid1c2Ugc3RyaWN0JztcblxuLy8gIHZhciBhID0gMDtcbiJdLCJmaWxlIjoibWFpbi5qcyJ9
